17372db3fb81078c4a5f8608ee06cd0895af35e6
Alejandro Jimenez <ajf@fastmail.com>
2021-10-02 12:58:53 -0500

    use a router

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(req, res) {
-  res.send('hola')
+app.use(router);
+
+router.get('/', function (req, res) {
+  res.send('Hola desde get')
 })
+
+router.post('/', function(req, res) {
+  res.send('Hola desde post')
+})
+
+/* app.use('/', function(req, res) {
+  res.send('hola')
+}) */
 
 app.listen(3000)
 console.log('la aplicación está escuchando en el puerto 3000')
