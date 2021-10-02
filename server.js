e095989720e9e57a3ac95b8982b18789b7f2f1c7
Alejandro Jimenez <ajf@fastmail.com>
2021-10-02 12:47:01 -0500

    simple app

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require('express')
+
+var app = express();
+
+app.use('/', function(req, res) {
+  res.send('hola')
+})
+
+app.listen(3000)
+console.log('la aplicación está escuchando en el puerto 3000')
