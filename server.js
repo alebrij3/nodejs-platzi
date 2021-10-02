a9767b9640f29d00b808175248f9cea701aca0e7
Alejandro Jimenez <ajf@fastmail.com>
2021-10-02 13:17:59 -0500

    uninstall nodemon from this project and install globally

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router();
 
 var app = express();
 
+app.use(bodyParser.json())
 app.use(router);
 
 router.get('/', function (req, res) {
   res.send('Hola desde get')
 })
 
-router.post('/', function(req, res) {
+router.delete('/', function(req, res) {
+  console.log(req.body)
   res.send('Hola desde post')
 })
 
