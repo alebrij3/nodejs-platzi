c700e32d4925710a9e64eacc35b3dd5b7ea34d48
Alejandro J <ajf@fastmail.com>
2021-10-13 14:09:37 -0500

    add user route

---
+++ components/user/network.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const response = require('../../network/response')
+const controller = require('./controller')
+
+const router = express.Router();
+
+router.post('/', function(req, res) {
+    controller.addUser(req.body.name)
+        .then(data => response.success(req, res, data, 201))
+        .catch(e => response.error(req, res, 'Error interno', 500, e))
+})
+
+module.exports = router;
